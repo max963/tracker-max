399aaf15b646c9ab0c4dcce436bfb3188c904f34
max963 <mg_morais@yahoo.com.br>
2022-04-25 18:18:49 -0300

    feat: componente de icones e botoes

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <button class="button" @click="finalizar" :disabled="!cronometroRodando"> -->
+  <button class="button">
+    <!-- <span class="icon">
+      <i class="fas fa-stop"></i>
+    </span> -->
+    <slot></slot>
+    <span>{{label}}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+
+
+export default defineComponent({
+  name: 'Button',
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
---
+++ src/components/Icons.vue
@@ -0,0 +1,20 @@
+<template>
+  <span class="icon">
+    <i v-if="icone===undefined"></i>
+    <i class="fas fa-stop" v-if="icone==='stop'"></i>
+    <i class="fas fa-play" v-if="icone==='play'"></i>
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Icons',
+  props: {
+    icone: {
+      type: String
+    }
+  }
+})
+</script>
