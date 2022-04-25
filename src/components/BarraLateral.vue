aac2c729352e30fba56de34407919fe357b86dc3
max963 <mg_morais@yahoo.com.br>
2022-04-25 17:40:22 -0300

    subindo o codigo do curso

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,51 @@
+<template>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="logo">
+    </h1>
+    <button class="button" @click="alterarTema">
+      {{textoDoBotao}}
+    </button>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'BarraLateral',
+  emits: ['aoTemaAlterado'],
+  data() {
+    return {
+      modoEscuroAtivo: false
+    }
+  },
+  computed: {
+    textoDoBotao() {
+      return this.modoEscuroAtivo ? "Desativar modo escuro" : "Ativar modo escuro";
+    }
+  },
+  methods: {
+    alterarTema() {
+      this.modoEscuroAtivo = !this.modoEscuroAtivo
+      this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
+    }
+  }
+})
+</script>
+
+<style scoped>
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5em;
+    height: auto;
+  }
+}
+</style>
---
+++ src/components/Box.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box has-text-weigth-bold" :style="estilos">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Box',
+  data () {
+    return {
+      estilos: {
+        backgroundColor: '#FAF0CA'
+      }
+    }
+  }
+})
+</script>
