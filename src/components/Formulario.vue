1e6c958ea79a6468b5f82a80cd2f7a57254a4fca
max963 <mg_morais@yahoo.com.br>
2022-04-26 17:09:23 -0300

    refactor: utilizar store e mutation

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="box formulario">
     <div class="columns">
-      <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+      <div class="column is-5" role="form" aria-label="Formulário para criação de uma nova tarefa">
         <input
           type="text"
           class="input"
@@ -9,34 +9,57 @@
           v-model="descricao"
         />
       </div>
-
+      <div class="column is-3">
+        <div class="select">
+          <select v-model="idProjeto">
+            <option value="">Selecione o projeto</option>
+            <option
+              :value="projeto.id"
+              v-for="projeto in projetos"
+              :key="projeto.id"
+            >
+              {{ projeto.nome }}
+            </option>
+          </select>
+        </div>
+      </div>
       <div class="column">
-        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa" />
+        <Temporizador @aoTemporizadorFinalizado="salvarTarefa" />
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
 import Temporizador from "./Temporizador.vue";
+import { useStore } from 'vuex';
+import { key } from '@/store'
 
 export default defineComponent({
   name: 'Formulario',
+  setup () {
+    const store = useStore(key)
+    return {
+      projetos: computed(() => store.state.projetos)
+    }
+  },
   components: {
     Temporizador
   },
   emits: ['aoSalvarTarefa'],
   data () {
     return {
-      descricao: ''
+      descricao: '',
+      idProjeto: ''
     }
   },
   methods: {
-    finalizarTarefa (tempoDecorrido: string): void {
+    salvarTarefa (tempoDecorrido: string): void {
       this.$emit('aoSalvarTarefa', {
         duracaoEmSegundos: tempoDecorrido,
-        descricao: this.descricao
+        descricao: this.descricao,
+        projeto: this.projetos.find(proj => this.idProjeto == proj.id)
       })
       this.descricao = ''
     }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { store, key } from './store/index';
 import { createApp } from 'vue'
 import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
@@ -5,4 +6,5 @@ import roteador from './roteador'
 
 createApp(App)
   .use(roteador)
+  .use(store, key)
   .mount('#app')
