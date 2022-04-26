a370bbb4760e482284afab5d4ebe00ef7ee5ee63
max963 <mg_morais@yahoo.com.br>
2022-04-26 14:43:55 -0300

    feat: add projects page

---
+++ src/views/Projetos.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: 'Projetos'
+})
+</script>
+
+<style>
+
+</style>
