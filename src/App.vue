39c36c881072c878b01566a21bbda29bc1660570
max963 <mg_morais@yahoo.com.br>
2022-04-26 14:16:34 -0300

    refactor: use router for load page tarefas

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,7 @@
       <BarraLateral @aoTemaAlterado="trocarTema" />
     </div>
     <div class="column is-three-quarter conteudo">
-      <Formulario @aoSalvarTarefa="salvarTarefa" />
-      <div class="lista">
-        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
-        <Box v-if="listaEstaVazia">
-          Parece que sua tarefa hj é dorme-office ne? 
-        </Box>
-      </div>
+      <router-view></router-view>
     </div>
   </main>
 </template>
@@ -18,33 +12,17 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue';
-import Box from './components/Box.vue';
-import Formulario from './components/Formulario.vue';
-import Tarefa from './components/Tarefa.vue';
-import ITarefa from './interfaces/iTarefa'
 export default defineComponent({
   components: { 
     BarraLateral, 
-    Formulario, 
-    Tarefa,
-    Box
   },
   data () {
     return {
-      tarefas: [] as ITarefa[],
       modoEscuroAtivo: false
     }
   },
   name: 'App',
-  computed: {
-    listaEstaVazia (): boolean {
-      return this.tarefas.length === 0
-    }
-  },
   methods: {
-    salvarTarefa (tarefa: ITarefa) {
-      this.tarefas.push(tarefa)
-    },
     trocarTema (modoEscuroAtivo: boolean) {
       this.modoEscuroAtivo = modoEscuroAtivo
     }
