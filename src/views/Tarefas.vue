fdf3cde526202c37e9fc6bca0b7eac1104dace89
max963 <mg_morais@yahoo.com.br>
2022-04-26 14:15:26 -0300

    feat: add view tarefas

---
+++ src/views/Tarefas.vue
@@ -0,0 +1,40 @@
+<template>
+  <Formulario @aoSalvarTarefa="salvarTarefa" />
+  <div class="lista">
+    <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+    <Box v-if="listaEstaVazia">
+      Parece que sua tarefa hj é dorme-office ne? 
+    </Box>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Box from '../components/Box.vue';
+import Formulario from '../components/Formulario.vue';
+import Tarefa from '../components/Tarefa.vue';
+import ITarefa from '../interfaces/iTarefa'
+export default defineComponent({
+  name: 'Tarefas',
+  components: { 
+    Formulario, 
+    Tarefa,
+    Box
+  },
+  data () {
+    return {
+      tarefas: [] as ITarefa[]
+    }
+  },
+  computed: {
+    listaEstaVazia (): boolean {
+      return this.tarefas.length === 0
+    }
+  },
+  methods: {
+    salvarTarefa (tarefa: ITarefa) {
+      this.tarefas.push(tarefa)
+    }
+  }
+});
+</script>
